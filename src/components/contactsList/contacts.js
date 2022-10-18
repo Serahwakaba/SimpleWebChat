9818385ffc8963c1ab7e9f53834d45b20f84d3ca
Serahwakaba <serahwakaba9@gmail.com>
2022-10-18 15:57:11 +0300

    contacts page design commit

--- src/components/contactsList/contacts.js
+++ src/components/contactsList/contacts.js
@@ -10,12 +10,22 @@ export default class Contacts extends Component{
         <i className="fa fa-plus"></i>
         <span>New conversation</span>
       </button>
+      <div className="chatlist__heading">
       <h2>Chats</h2>
       <button className="btn-nobg">
         <i className="fa fa-ellipsis-h"></i>
       </button>
+    </div>
+    <div className="chatList__search">
+    <div className="search_wrap">
+      <input type="text" placeholder="Search Here" required />
+      <button className="search-btn">
+        <i className="fa fa-search"></i>
+      </button>
+    </div>
+  </div>
+    </div>
       
-      </div>
           );
     }
 }
