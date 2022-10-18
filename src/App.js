13407ce743283f2bbd045e9b609a8336e0d5940b
Serahwakaba <serahwakaba9@gmail.com>
2022-10-18 15:09:07 +0300

    conversation body design commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from "./components/navigation/navigation";
+import ConversationBody from "./components/conversationBody/chat";
+
 
 
 function App() {
   return (
     <div className="__main">
     <Navigation />
+    <ConversationBody />
 
     </div>
   );
---
+++ src/components/conversationBody/chat.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./chat.css";
+
+
+export default class ConversationBody extends Component{
+
+    render() {
+        return (
+          <div className="main__chat">
+           
+          </div>
+        );
+      }
+}
