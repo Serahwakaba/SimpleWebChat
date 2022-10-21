e855ab0d9c3f34e46a296af80975e0195f8fc181
Serahwakaba <serahwakaba9@gmail.com>
2022-10-21 10:38:33 +0300

    function displaying conversation commit

--- src/components/chatContent/chatContent.js
+++ src/components/chatContent/chatContent.js
@@ -49,6 +49,49 @@ export default class ChatContent extends Component {
           type: "",
           msg: "what plan mate?",
         },
+        {
+        key: 7,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
+        type: "other",
+        msg: "I'm taliking about the tutorial",
+      },
+    ];
+  
+
+        constructor(props) {
+            super(props);
+            this.state = {
+              chat: this.chatItms,
+              msg: "",
+            };
+          }
+        
+          scrollToBottom = () => {
+            this.messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+          };
+          componentDidMount() {
+            window.addEventListener("keydown", (e) => {
+              if (e.keyCode == 13) {
+                if (this.state.msg != "") {
+                  this.chatItms.push({
+                    key: 1,
+                    type: "",
+                    msg: this.state.msg,
+                    image:
+                      "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
+                  });
+                  this.setState({ chat: [...this.chatItms] });
+                  this.scrollToBottom();
+                  this.setState({ msg: "" });
+                }
+              }
+            });
+            this.scrollToBottom();
+          }
+          onStateChange = (e) => {
+            this.setState({ msg: e.target.value });
+          };
 
     render(){
         return (
